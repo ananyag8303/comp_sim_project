08e01ab886829dba4e0fe5da0aa25a1e818c04ce
ananyag8303 <agolikere@gmail.com>
2023-06-25 15:43:46 +0530

    Create paramaters.py

---
+++ paramaters.py
@@ -0,0 +1,76 @@
+''' main paramter file for the entire project'''
+#note SI units here have been converted into natural units of AU, Solar mass and year. 
+#number of iterations
+100000
+# timestep (earth yr)
+# 300 sec good for mars moons
+0.001
+# Gravitational constant(AU^3 M⊙^-1 yr^-2)
+# M⊙ = 332946 M⊕
+# 39.5 / 332946
+1.18638e-4
+# sun data
+# name
+Sun
+# mass M⊕
+332946
+# orbital radius AU
+0.0
+# colour
+gold
+# planet data
+# mercury data
+# name
+Mercury
+# mass M⊕
+0.06
+# orbital radius AU
+0.387
+# colour
+gray
+# venus data
+# name
+Venus
+# mass M⊕
+0.82
+# orbital radius AU
+0.723
+# colour
+orange
+# earth data
+# name
+Earth
+# mass M⊕
+1
+# orbital radius AU
+1.0
+# colour
+deepskyblue
+#Satellite
+#name
+Satellite
+#mass 
+1.2e-21
+# orbital radius AU
+1.000000001
+#colour
+mistyrose
+# mars data
+# name
+Mars
+# mass M⊕
+0.11
+# orbital radius AU
+1.524
+# colour
+firebrick
+# outer planets
+# jupiter data
+# name
+Jupiter
+# mass M⊕
+318
+# orbital radius AU
+5.2
+# colour
+brown
